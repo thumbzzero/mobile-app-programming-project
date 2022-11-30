6497ef9751fe97d7b4bfc4206e2294425061271a
thumbzzero <thumbzzero@gmail.com>
2022-12-01 03:55:52 +0900

    dashboard menu layout

---
+++ app/src/main/java/com/example/mobileappproject/common/LmsMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobileappproject.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobileappproject.R
+
+class LmsMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lms_menu)
+    }
+}
