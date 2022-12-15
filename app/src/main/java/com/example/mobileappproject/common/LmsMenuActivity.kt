6debfca06d136337c73c274397d379f24ea3c81e
Aritta <suho6347@naver.com>
2022-12-15 15:55:43 +0900

    LMS 메뉴에서 강의자료실 불러오기

--- app/src/main/java/com/example/mobileappproject/common/LmsMenuActivity.kt
+++ app/src/main/java/com/example/mobileappproject/common/LmsMenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobileappproject.common
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileappproject.databinding.ActivityLmsMenuBinding
 
@@ -11,5 +13,10 @@ class LmsMenuActivity : AppCompatActivity() {
         setContentView(binding.root)
         val course_name = intent.getStringExtra("course_name")
         binding.courseName.text = "${course_name}"
+
+        binding.menuTextbook.setOnClickListener {
+            val intent = Intent(this, LmsLectureMaterialActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
