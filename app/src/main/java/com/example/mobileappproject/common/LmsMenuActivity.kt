669bded24f897e186220176b04460d349553d95e
Hong <bayyyy@naver.com>
2022-12-15 15:59:36 +0900

    lms menuActivity setClickListener

--- app/src/main/java/com/example/mobileappproject/common/LmsMenuActivity.kt
+++ app/src/main/java/com/example/mobileappproject/common/LmsMenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileappproject.common
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileappproject.databinding.ActivityLmsMenuBinding
@@ -11,5 +12,31 @@ class LmsMenuActivity : AppCompatActivity() {
         setContentView(binding.root)
         val course_name = intent.getStringExtra("course_name")
         binding.courseName.text = "${course_name}"
+
+        val subjcetCode = intent.getStringExtra("subjcetcode")//todo 가져오기
+
+        binding.menuNotification.setOnClickListener {
+            //공지
+            val intent = Intent(this, LmsNoticeActivity::class.java)
+
+            intent.putExtra("subjcetCode","CLTR0011-001")
+            startActivity(intent)
+        }
+
+        binding.menuTextbook.setOnClickListener {
+            //강의자료실
+            val intent = Intent(this, LmsLectureMaterialActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.menuHomework.setOnClickListener {
+            //과제
+        }
+
+        binding.menuAttendance.setOnClickListener {
+            //스마트출결
+        }
+
+
     }
 }
